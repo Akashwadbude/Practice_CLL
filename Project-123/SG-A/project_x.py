b565936df51a22490b9e99338d562d1fd5c1f5f3
Akash Wadbude <akashwadbude.111@gmail.com>
2023-10-15 17:36:33 +0530

    update in projectx

--- Project-123/SG-A/project_x.py
+++ Project-123/SG-A/project_x.py
@@ -1,6 +1,6 @@
 import time
 
 while True:
-    print("This script is running continuously.")
+    print("This Script is running continuously.")
     time.sleep(1)  # Wait for 1 second before the next iteration
 
